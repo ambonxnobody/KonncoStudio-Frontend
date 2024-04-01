548cb8158ffe0725366b2bfc4c3b14a77a742d8d
Hafid Dian Nurfaujan Ahat <ambonxnobody.15072001@gmail.com>
2024-04-01 23:04:37 +0700

    Fix Error Linter

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -4,7 +4,8 @@ import { useAuth } from '@/hooks/auth'
 import Navigation from '@/app/(app)/Navigation'
 import Loading from '@/app/(app)/Loading'
 
-const AppLayout = ({ children, header }) => {
+// header
+const AppLayout = ({ children }) => {
     const { user } = useAuth({ middleware: 'auth' })
 
     if (!user) {
